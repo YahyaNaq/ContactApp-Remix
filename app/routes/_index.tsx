b62133f6c41d672726b987c1fb6520d398c157e8
Yahya Faheem <yahya.faheem@trax.pk>
2024-10-27 19:41:09 +0500

    done

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+    return (
+      <p id="index-page">
+        This is a remix contacts app made by Yahya Faheem.
+        <br />
+        Check out{" "}
+        <a href="https://remix.run">the docs at remix.run</a>.
+      </p>
+    );
+  }
+  
